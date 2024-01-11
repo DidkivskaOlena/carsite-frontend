d362fe739dfd51245cf73b20655e4990873fe6d3
DidkivskaOlena <didkivskalyona@gmail.com>
2024-01-11 17:14:05 +0200

    Update CardItem.jsx

--- src/components/CarCatalogue/CardItem.jsx
+++ src/components/CarCatalogue/CardItem.jsx
@@ -6,14 +6,14 @@ export function CardComponent({ data }) {
   return (
     <div className={style.cards}>
 
+      <div className={style.cardHeader}>
+        <p className={style.nameCard}>In good hands</p>
+      </div>
+
       <div>
         <a href="#" className={style.favoritesIcon}>
           <img src={data.primary_photo_url} width = "260"/>
         </a>
-      </div>
-
-      <div className={style.cardHeader}>
-        <p className={style.nameCard}>In good hands</p>
       </div>
 
       <div className={style.inCard}>
