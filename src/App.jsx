bea18e56df3f2cedd29f6d9a370c335d848f39a9
DidkivskaOlena <didkivskalyona@gmail.com>
2024-02-05 17:33:27 +0200

    add validate login
    сделать проверку валидации и добавить ошибку в нужное поле

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const Favorites = lazy(()=> import("./pages/Favorites/FavoritesPage"))
 const FAQ = lazy(()=> import("./pages/FAQ/FAQPage"))
 const Contacts = lazy(()=> import("./pages/Contacts/ContactPage"))
 const LoginPage = lazy(() => import("./pages/Login/LoginPage"))
+const NotFoundPage = lazy(() => import("./pages/NotFound/NotFoundPage"))
 
 function App() {
   return (
@@ -43,6 +44,7 @@ function App() {
                 <PrivateRoute redirectTo="/login" component={<AdminPage/>}/>
               }/>
             </Route>
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
           </Suspense>
         </BrowserRouter>
